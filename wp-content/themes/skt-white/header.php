3d782cc3290eb3180892c3f7029d9371a94df1b3
Irina Magdici <irina.magdici@xivic.com>
2015-07-23 12:56:19 +0300

    Add GTM
    GTM snippet add

--- wp-content/themes/skt-white/header.php
+++ wp-content/themes/skt-white/header.php
@@ -27,6 +27,15 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-58LSLV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-58LSLV');</script>
+<!-- End Google Tag Manager -->
 
 <?php if ( (of_get_option('innerpageslider', true) != 'hide') || is_home() || is_front_page() ) { ?>
 	<div class="slider-main">
